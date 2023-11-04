cf99b24b769e1677d95cb463f4fa35d807c563c7
WallaceMariano <wallacewvinfo@gmail.com>
2023-11-04 10:17:02 -0300

    :sparkles: feat: Solving troco algorithm

---
+++ troco.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    float price;
+    int amount;
+    float value;
+    float change;
+
+    printf("Preco unitario do produto: ");
+    scanf("%f", &price);
+
+    printf("Quantidade comprada: ");
+    scanf("%d", &amount);
+
+    printf("Dinheiro recebido: ");
+    scanf("%f", &value);
+
+    change = value - (price * amount);
+
+    printf("TROCO = %.2f", change);
+
+    return 0;
+}
